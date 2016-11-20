4901b253b317d1d9d4c3b7f06c9edbbab42578a1
Basile BELDAME <basile.beldame@supdeweb-paris.com>
2016-11-20 21:16:05 +0100

    ok

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bbeldame <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 19:08:31 by bbeldame          #+#    #+#             */
-/*   Updated: 2016/11/19 19:14:48 by bbeldame         ###   ########.fr       */
+/*   Updated: 2016/11/20 21:13:41 by bbeldame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
-# define BUFF_SIZE 4096
+# define BUFF_SIZE 5000
 
 int		get_next_line(const int fd, char **line);
 
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,24 +6,29 @@
 /*   By: bbeldame <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 19:07:30 by bbeldame          #+#    #+#             */
-/*   Updated: 2016/11/20 17:44:47 by bbeldame         ###   ########.fr       */
+/*   Updated: 2016/11/20 20:08:38 by bbeldame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 int		main(void)
 {
 	int		fd;
 	char	*str;
+	int		i;
 
 	fd = open("file.txt", O_RDONLY);
 	if (fd == -1)
 		return (0);
+	i = 0;
 	while (get_next_line(fd, &str))
 	{
-//		ft_putstr(str);
+		i++;
+		printf("Loop %d\n", i);
+		ft_putstr(str);
 		ft_putchar('\n');
 	}
 	if (close(fd) == -1)
