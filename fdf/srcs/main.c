fdfcad726769919b6031d65e67b9a5ed533a195c
Basile BELDAME <basile.beldame@supdeweb-paris.com>
2017-03-26 19:50:42 +0200

    ok

--- fdf/srcs/main.c
+++ fdf/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbeldame <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bbeldame <bbeldame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/15 20:00:22 by bbeldame          #+#    #+#             */
-/*   Updated: 2017/01/30 19:56:16 by bbeldame         ###   ########.fr       */
+/*   Updated: 2017/03/26 19:49:21 by bbeldame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int			main(int ac, char **av)
 	t_env	*e;
 
 	if (ac != 2)
-		err_found("usage : fdf input_map");
+		err_found("usage : fdf input_map.fdf");
+	if (ft_strncmp(ft_strrev(av[1]), "fdf.", 4) != 0)
+		err_found("usage : fdf input_map.fdf");
+	ft_strrev(av[1]);
 	if (!(e = initenv(av[1])))
 		err_found("a problem occured during the parsing.");
 	rot_x(e, X_DEF);
